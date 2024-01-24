9437c2a0f4a27e14e8d17dff353622d62248dba9
heeneth <heeneth123@gmail.com>
2024-01-24 09:48:01 +0530

    added uc4

--- employeewages.sh
+++ employeewages.sh
@@ -1,19 +1,22 @@
-
 ispresent=1
-payperhover=20
+payperhour=20
 day=8
 
+checkAtt=$((RANDOM%2))
 
-checkAtt=$((RANDOME%2))
-
-
-if [ $ispresent == $checkAtt ]
-then
-echo "Employ is present"
-else
-echo "employe is absent"
-fi
-
-pay=$(($payperhover * $day))
-echo $pay
+case $checkAtt in
+    0)
+        echo "Employee is absent"
+        ;;
+    1)
+        echo "Employee is present"
+        # Assuming part-time hours is 8 for calculation
+        partTimeHours=8
+        pay=$(($payperhour * $partTimeHours * $day))
+        echo "Total Wage: $pay"
+        ;;
+    *)
+        echo "Invalid value for checkAtt"
+        ;;
+esac
 
